f19866b88eb5a2851c84773962afb997dc7d10e3
Akash R Chandran <chandranrakash@gmail.com>
2025-04-01 00:26:21 +0530

    still in the proccessing phase

---
+++ Parallel processing/Multithreaded step/SalesDataLoader-Multithreaded/src/main/java/in/akashrchandran/salesdataloader/dto/SalesDataDto.java
@@ -0,0 +1,22 @@
+package in.akashrchandran.salesdataloader.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SalesDataDto {
+    private Long index;
+    private String invoiceNo;
+    private String stockCode;
+    private String description;
+    private Integer quantity;
+    private String invoiceDate;
+    private BigDecimal unitPrice;
+    private String customerId;
+    private String country;
+}
---
+++ chunk-oriented/SalesDataLoader/src/main/java/in/akashrchandran/salesdataloader/SalesDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package in.akashrchandran.salesdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDataLoaderApplication.class, args);
+	}
+
+}
